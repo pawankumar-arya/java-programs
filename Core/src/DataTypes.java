3cb4885ce275e3ca513bf1cb2a406ea4b8e0e055
pawankumar-arya <pawanarya6215@gmail.com>
2026-01-17 01:57:32 +0530

    first commit

---
+++ Core/src/DataTypes.java
@@ -0,0 +1,16 @@
+ class DataTypes {
+    public static void main(String[] args) {
+        int num = 10 ;
+        char ch = 'c' ;
+        String str = "hello" ;
+        float ft = 2.22f ;
+        double db = 2.25 ;
+        boolean bl = true ;
+        System.out.println(num);
+        System.out.println(ch);
+        System.out.println(str);
+        System.out.println(ft);
+        System.out.println(db);
+        System.out.println(bl);
+    }
+}
