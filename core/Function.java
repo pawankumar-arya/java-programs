de350b083a036fb56b27ada6e955fcd147d73ed5
pawankumar-arya <pawanarya6215@gmail.com>
2026-01-18 20:42:03 +0530

    java basics

---
+++ core/Function.java
@@ -0,0 +1,9 @@
+public class Function {
+  public static void main(String[] args) {
+    greet("Alice");
+    greet("Bob");
+  }
+  static void greet(String name) {
+    System.out.println("Hello, " + name + "!");
+  }
+}
---
+++ core/Operator.java
@@ -0,0 +1,9 @@
+public class Operator{
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int sum = a + b; // Using the addition operator
+    
+    System.out.println("Sum: " + sum);
+  }
+}
