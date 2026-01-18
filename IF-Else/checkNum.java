de1e17e8d209e758cebfec1a6fb66d36681b7c52
pawankumar-arya <pawanarya6215@gmail.com>
2026-01-18 20:09:19 +0530

    if else

---
+++ IF-Else/checkNum.java
@@ -0,0 +1,13 @@
+// package IF-Else;
+public class checkNum {
+  public static void main(String[] args) {
+    int number = 10;
+    if (number > 0) {
+      System.out.println(number + " is a positive number.");
+    } else if (number < 0) {
+      System.out.println(number + " is a negative number.");
+    } else {
+      System.out.println("The number is zero.");
+    }
+  }
+}
